2900381595b0b02ab2d9c8feec7361bd73b2d755
prasanna <prasanna.gunda06@gmail.com>
2023-05-31 09:01:22 +0000

    terraform script for ec2 instance

--- route53/main.tf
+++ route53/main.tf
@@ -1,5 +1,5 @@
 resource "aws_route53_record" "record" {
-  zone_id = Z07471731U7WCB4GJO8FS
+  zone_id = "Z07471731U7WCB4GJO8FS"
   name    = "{var.component}-dev.devopsprasanna1.store"
   type    = "A"
   ttl     = 30
