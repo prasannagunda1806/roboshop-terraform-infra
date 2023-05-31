80251340e104e838fe2ded83a790a6a602d13fed
prasanna <prasanna.gunda06@gmail.com>
2023-05-31 08:42:07 +0000

    terraform script for ec2 instance

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 module "ec2" {
     source = "./ec2"
     for_each = var.instances
-    instance_type = each.value[ "type"]
-    component = each.value[ "name " ]
+    instance_type = each.value[ "type" ]
+    component = each.value[ "name" ]
     sg_id = module.sg.sg_id
 }
 
