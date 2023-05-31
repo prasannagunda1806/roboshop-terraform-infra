959ea2419ddf6dd96018bc663e20d8d1544a050c
prasanna <prasanna.gunda06@gmail.com>
2023-05-31 08:56:44 +0000

    terraform script for ec2 instance

--- ec2/main.tf
+++ ec2/main.tf
@@ -6,7 +6,7 @@ data "aws_ami" "ami" {
 
 
 resource "aws_instance" "ec2" {
-  ami                     = data.aws_ami.ami.ami_id 
+  ami                     = data.aws_ami.ami.id 
   instance_type           = [var.instance_type]
   vpc_security_group_ids  = var.sg_id
 
