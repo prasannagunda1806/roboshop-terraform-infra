69b96d30b737514f3aba3c6d2d582d556ce3db30
prasanna <prasanna.gunda06@gmail.com>
2023-05-31 08:44:34 +0000

    terraform script for ec2 instance

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 module "ec2" {
     source = "./ec2"
     for_each = var.instances
-    instance_type = each.value[ "type" ]
-    component = each.value[ "name" ]
+    instance_type = each.value["type"]
+    component = each.value["name"]
     sg_id = module.sg.sg_id
 }
 
@@ -13,8 +13,8 @@ module "sg" {
 module "route53" {
     for_each = var.instances
     source = "./route53"
-    private_ip = module.ec2.[each.value[ " name " ]].private_ip
-    component = each.value[ "name" ]
+    private_ip = module.ec2.[each.value["name"]].private_ip
+    component = each.value["name"]
 }
 
 output ec2 {
