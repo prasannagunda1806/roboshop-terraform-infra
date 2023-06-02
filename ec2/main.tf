242cfd8969df04b86f8155c7e5a304250d2a2db2
prasanna <prasanna.gunda06@gmail.com>
2023-06-02 15:33:25 +0000

    u

--- ec2/main.tf
+++ ec2/main.tf
@@ -22,7 +22,7 @@ resource "null_resource" "provisioner" {
   provisioner "remote-exec" {
     connection {
       host     = aws_instance.ec2.public_ip
-      user     = "centos"
+      user     = "root"
       password = "DevOps321"
     }
 
