c27a68ee4b00313de4b99a3409021b71f1d2974e
prasanna <prasanna.gunda06@gmail.com>
2023-06-02 07:12:44 +0000

    update

--- route53/main.tf
+++ route53/main.tf
@@ -1,6 +1,6 @@
 resource "aws_route53_record" "record" {
   zone_id = "Z07471731U7WCB4GJO8FS"
-  name    = "{var.component}-dev.devopsprasanna1.store"
+  name    = "${var.component}-dev.devopsprasanna1.store"
   type    = "A"
   ttl     = 30
   records = [var.private_ip]
